4775d0800ad0cc3b0e61a4cd80c15bfca087a95e
Arūnas Smaliukas <arunas@smala.lt>
2016-12-09 13:36:05 +0200

    Update universal.js include strategy

--- trunk/include/mailerlite-form.php
+++ trunk/include/mailerlite-form.php
@@ -217,12 +217,12 @@ function mailerlite_universal() { ?>
     <!-- MailerLite Universal -->
     <script>
         (function(m,a,i,l,e,r){m['MailerLiteObject']=e;function f(){
-            var c={a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
-            f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=f.q;r=a.createElement(i);
-            var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l;_.parentNode.insertBefore(r,_);
-        })(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
+        var c={a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
+        f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
+        var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?'+(~~(new Date().getTime()/10000000));
+        _.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
 
-        ml('accounts', '<?php echo get_option('account_id'); ?>', '<?php echo get_option('account_subdomain'); ?>', 'load');
+        var ml_account = ml('accounts', '<?php echo get_option('account_id'); ?>', '<?php echo get_option('account_subdomain'); ?>', 'load');
     </script>
     <!-- End MailerLite Universal -->
 <?php }
