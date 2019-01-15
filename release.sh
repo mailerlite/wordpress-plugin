e0bff9e0a4df06aa3b9f5e994ee5a8ab88d6fe9c
dominykastijunaitis <dominykas@tijunaitis.lt>
2019-01-15 19:16:38 +0200

    Add release script

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+latest="$(cat latest_version.txt)"
+
+echo latest version was: $latest
+echo enter version like: 1.2.3
+read release
+
+echo releasing: $release
+
+# write the latest version to the file
+printf $release > latest_version.txt
+
+# make sure branches are up to date
+git checkout develop || exit 1
+git pull || exit 1
+git checkout master || exit 1
+git pull || exit 1
+
+# merge develop into master
+git merge develop --commit --no-edit || exit 1
+
+git tag -a $release -m "$release release" || exit
+
+git push --follow-tags || exit 1
+
+# back to develop branch
+git checkout develop || exit 1
