792b98616f8ce8a707da56f7da3354d7c967b1e3
johnkelesidis <john@mailerlite.com>
2021-03-10 14:29:50 +0200

    Revert "new method to embed webforms on wp"
    This reverts commit b0bb4d934e8da933fdd5d5fe7ac40cda67419105.

--- trunk/include/templates/forms/embedded_form.php
+++ trunk/include/templates/forms/embedded_form.php
@@ -1,11 +1,1 @@
-<?php if (get_option('account_id') && get_option('account_subdomain')) { ?>
-
-        <div class="ml-form-embed"
-             data-account="<?php echo get_option('account_id') . ':' . get_option('account_subdomain'); ?>"
-             data-form="<?php echo $form_data['id'] . ':' . $form_data['code']; ?>">
-        </div>
-
-<?php } else { ?>
-
-    <script type="text/javascript" src="https://static.mailerlite.com/data/webforms/<?php echo $form_data['id']; ?>/<?php echo $form_data['code']; ?>.js?v=<?php echo time(); ?>"></script>
-<?php } ?>
+<script type="text/javascript" src="https://static.mailerlite.com/data/webforms/<?php echo $form_data['id']; ?>/<?php echo $form_data['code']; ?>.js?v=<?php echo time(); ?>"></script>
