7aecc5e63d7d0ce1220592e4ac8946f3b3a745f9
Dino <dinomh@live.com>
2021-03-11 13:38:48 +0100

    master to main

--- release.sh
+++ release.sh
@@ -7,15 +7,15 @@ echo releasing: $release
 # make sure branches are up to date
 git checkout develop || exit 1
 git pull || exit 1
-git checkout master || exit 1
+git checkout main || exit 1
 git pull || exit 1
 
-# merge develop into master
+# merge develop into main
 git merge develop --commit --no-edit || exit 1
 
 git tag -a $release -m "$release release" || exit
 
 git push --follow-tags || exit 1
 
-# back to master branch
-git checkout master || exit 1
+# back to main branch
+git checkout main || exit 1
